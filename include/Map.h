b85ab969dac34e56100a55c7cbe6438bb742ca4b
Hod Vaknin <hodvak@gmail.com>
2023-04-26 00:09:20 +0300

    fix Map as 2 files

--- include/Map.h
+++ include/Map.h
@@ -3,89 +3,21 @@
 #include <SFML/Graphics.hpp>
 #include "MapVector.h"
 
-class Map
+class Map : public sf::Drawable
 {
 public:
-
+    //todo: maybe change from consts to the size of the mask
     static const int WIDTH = 1080;
     static const int HEIGHT = 720;
 
-    Map(sf::Image mask, sf::Image sky, sf::Image ground) :
-            m_mask(mask),
-            m_sky(sky),
-            m_ground(ground)
-    {
-        m_display.create(WIDTH, HEIGHT);
-        for (int i = 0; i < WIDTH; ++i)
-        {
-            for (int j = 0; j < HEIGHT; ++j)
-            {
-                sf::Color maskColor = mask.getPixel(i, j);
-                sf::Color skyColor = sky.getPixel(i, j);
-                sf::Color groundColor = ground.getPixel(i, j);
-                if (maskColor == sf::Color::Black)
-                {
-                    m_display.setPixel(i, j, skyColor);
-                }
-                else
-                {
-                    m_display.setPixel(i, j, groundColor);
-                }
-            }
+    Map(const sf::Image &mask, const sf::Image &sky, const sf::Image &ground);
 
-        }
-    }
+    const sf::Image *getMask() const;
 
-    sf::Color getSkyColor(int x, int y)
-    {
-        return m_sky.getPixel(x, y);
-    }
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
-    sf::Color getGroundColor(int x, int y)
-    {
-        return m_ground.getPixel(x, y);
-    }
-
-    sf::Color getMaskColor(int x, int y)
-    {
-        return m_mask.getPixel(x, y);
-    }
-
-    sf::Image *getMask()
-    {
-        return &m_mask;
-    }
-
-    sf::Image *getSky()
-    {
-        return &m_sky;
-    }
-
-    sf::Image *getGround()
-    {
-        return &m_ground;
-    }
-
-    sf::Image *getDisplay()
-    {
-        return &m_display;
-    }
-
-    void drawCircle(MapVector pos, int radius)
-    {
-        for (int i = 0; i < WIDTH; ++i)
-        {
-            for (int j = 0; j < HEIGHT; ++j)
-            {
-                if ((pos-MapVector(i,j)).getMagnitude() < radius)
-                {
-                    m_mask.setPixel(i, j, sf::Color::Black);
-                    m_display.setPixel(i, j, getSkyColor(i, j));
-                }
-            }
-        }
-    }
-
+    void drawCircle(MapVector pos, int radius);
+    
 private:
     sf::Image m_mask;
     sf::Image m_sky;
---
+++ src/Map.cpp
@@ -0,0 +1,62 @@
+#include "Map.h"
+
+Map::Map(const sf::Image &mask, const sf::Image &sky, const sf::Image &ground) :
+        m_mask(mask),
+        m_sky(sky),
+        m_ground(ground)
+{
+
+    m_display.create(WIDTH, HEIGHT);
+    for (int i = 0; i < WIDTH; ++i)
+    {
+        for (int j = 0; j < HEIGHT; ++j)
+        {
+            sf::Color maskColor = mask.getPixel(i, j);
+            if (maskColor == sf::Color::Black)
+            {
+                sf::Color skyColor = sky.getPixel(i, j);
+                m_display.setPixel(i, j, skyColor);
+            }
+            else
+            {
+                sf::Color groundColor = ground.getPixel(i, j);
+                m_display.setPixel(i, j, groundColor);
+            }
+        }
+    }
+}
+
+void Map::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    sf::Sprite sprite;
+    sf::Texture texture;
+    texture.loadFromImage(m_display);
+    sprite.setTexture(texture);
+    target.draw(sprite, states);
+}
+
+void Map::drawCircle(MapVector pos, int radius)
+{
+    int radiusSquared = radius * radius;
+    for (int i = -radius; i < radius; ++i)
+    {
+        for (int j = -radius; j < radius; ++j)
+        {
+            if (i * i + j * j < radiusSquared)
+            {
+                int x = (int) pos.x + i;
+                int y = (int) pos.y + j;
+                if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+                {
+                    m_mask.setPixel(x, y, sf::Color::Black);
+                    m_display.setPixel(x, y, m_sky.getPixel(x, y));
+                }
+            }
+        }
+    }
+}
+
+const sf::Image *Map::getMask() const
+{
+    return &m_mask;
+}
