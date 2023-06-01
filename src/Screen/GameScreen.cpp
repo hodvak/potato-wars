385c45e3ddb61408a210a0523b30df90f81bf9ff
Hod Vaknin <hodvak@gmail.com>
2023-06-01 21:06:37 +0300

    add commnet about online game

--- src/Screen/GameScreen.cpp
+++ src/Screen/GameScreen.cpp
@@ -35,6 +35,10 @@ std::unique_ptr<Screen> GameScreen::run(sf::RenderWindow &window)
         }
         sf::Time delta = clock.restart();
         m_game.update(delta);
+        // maybe for online game use this: 
+        // m_game.update(sf::seconds(1.0f/60.0f));
+        // all computers will end with the same result but the slower computers
+        // will see the game slower (and update when they're done updating)
         
         window.clear();
         window.draw(m_game);
