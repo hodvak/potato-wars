a81b86abbbe29903d15941605a83811f27af5d56
Hod Vaknin <hodvak@gmail.com>
2023-06-11 10:02:44 +0300

    fix: fix the rifle draw
    the error was that the rifle was shown upside down

--- src/Weapon/Textures/RifleTexture.cpp
+++ src/Weapon/Textures/RifleTexture.cpp
@@ -5,13 +5,13 @@
 RifleTexture::RifleTexture(PlayerColor color,
                            MapVector position,
                            MapVector aimPosition,
-                           float radius):
-    m_color(color),
-    m_position(position),
-    m_aimPosition(aimPosition),
-    m_radius(radius)
+                           float radius) :
+        m_color(color),
+        m_position(position),
+        m_aimPosition(aimPosition),
+        m_radius(radius * 1.5f)
 {
-    
+
 }
 
 void RifleTexture::setPosition(MapVector position)
@@ -27,21 +27,32 @@ void RifleTexture::setAimPosition(MapVector aimPosition)
 void RifleTexture::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
     sf::Sprite aim;
-    aim.setTexture(*resources_manager::getTexture("resources/Images/Textures/Aim.png"));
-    aim.setOrigin(aim.getTexture()->getSize().x / 2, aim.getTexture()->getSize().y / 2);
+    aim.setTexture(*resources_manager::getTexture(
+            "resources/Images/Textures/Aim.png"));
+    aim.setOrigin(aim.getTexture()->getSize().x / 2,
+                  aim.getTexture()->getSize().y / 2);
     aim.setPosition(m_aimPosition);
     target.draw(aim, states);
-    
+
     sf::Sprite sprite;
-    sprite.setTexture(*resources_manager::getTexture("resources/Images/Textures/rifles.png"));
+    sprite.setTexture(*resources_manager::getTexture(
+            "resources/Images/Textures/rifles.png"));
     int left = (int) (m_color);
     sprite.setTextureRect(sf::IntRect(left * 100, 0, 100, 100));
+
     sprite.setOrigin(50, 50);
     sprite.setScale(m_radius / 50, m_radius / 50);
-    
+
     sprite.setPosition(m_position);
-    sprite.setRotation((m_aimPosition - m_position).getAngle() * 180 / std::numbers::pi);
-    
+    float angle = (m_aimPosition - m_position).getAngle();
+    sprite.setRotation(angle * 180 / std::numbers::pi_v<float>);
+
+    if (angle > std::numbers::pi_v<float> / 2 ||
+        angle < -std::numbers::pi_v<float> / 2)
+    {
+        sprite.setScale(m_radius / 50, -m_radius / 50);
+    }
+
     target.draw(sprite, states);
-    
+
 }
